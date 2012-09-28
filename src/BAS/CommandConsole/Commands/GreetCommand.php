945fd79750205966c05c5b11707d62d541bd9c14
lumberjacked <lumberjacked.again@gmail.com>
2012-09-28 14:06:46 -0500

    got everything autoloading and the console file working with a demo GreetCommand

--- src/BAS/CommandConsole/Commands/GreetCommand.php
+++ src/BAS/CommandConsole/Commands/GreetCommand.php
@@ -5,7 +5,7 @@ use Symfony\Component\Console\Command\Command,
     Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputInterface,
     Symfony\Component\Console\Input\InputOption,
-    Symfony\Component\Console\OutputInterface;
+    Symfony\Component\Console\Output\OutputInterface;
 
 class GreetCommand extends Command 
 {
@@ -15,21 +15,22 @@ class GreetCommand extends Command
              ->setDescription('Greet Someone')
              ->addArgument('name', InputArgument::OPTIONAL, 'Who do you think you are?')
              ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters');
+       
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, $output)
     {
         $name = $input->getArgument('name');
         if($name) {
             $text = 'Looser' . $name;
         } else {
-            $text = 'Your still a Looser';
+            $text = 'Your still a Looser ';
         }
 
         if($input->getOption('yell')){
             $text = strtoupper($text);
         }
 
-        $output->writeln($text);
+        $output->writeln("<error>{$text}</error>");
     }
 }
