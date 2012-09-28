1353600b775c4a6f121cda94cbb957bfef9f91b2
lumberjacked <lumberjacked.again@gmail.com>
2012-09-28 13:30:50 -0500

    Moved console file to root of directory

---
+++ src/BAS/CommandConsole/Commands/GreetCommand.php
@@ -0,0 +1,35 @@
+<?php 
+namespace BAS\CommandConsole\Commands;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\OutputInterface;
+
+class GreetCommand extends Command 
+{
+    protected function configure()
+    {
+        $this->setName('demo:greet')
+             ->setDescription('Greet Someone')
+             ->addArgument('name', InputArgument::OPTIONAL, 'Who do you think you are?')
+             ->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if($name) {
+            $text = 'Looser' . $name;
+        } else {
+            $text = 'Your still a Looser';
+        }
+
+        if($input->getOption('yell')){
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
