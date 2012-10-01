71594a70a93f227e0b08e41d6334f807350c935a
lumberjacked <lumberjacked.again@gmail.com>
2012-10-01 16:19:04 -0500

    Starting to abstract out the configure this way all commands can use the helper rather than duplicating the code

---
+++ src/BAS/zf2/Commands/RouteCommand.php
@@ -0,0 +1,52 @@
+<?php 
+namespace BAS\zf2\Commands;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface,
+    BAS\zf2\Helpers\ConfigHelper;
+
+class RouteCommand extends Command 
+{
+    protected $config; 
+
+    protected function configure()
+    {
+        $this->config = new ConfigHelper();
+        
+        $this->setName('route:add')->setDescription($this->getDescript())
+             ->addArgument('{RouteName}', InputArgument::REQUIRED, 'Choose a name for this route.')
+             ->addOption('type', null, InputOption::VALUE_REQUIRED, 'Choose a Route Type!', 'Segment');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+    }
+
+    protected function getDescript() {
+        return <<<DEC
+Add a Route to your Project (Routes are case - insensitive)
+Possible Routes
+--- Hostname
+--- Literal
+--- Method
+--- Part
+--- Regex
+--- Scheme
+--- Segment (Default)
+--- Query
+DEC;
+
+    }
+
+    protected function setWorkingDir($wd = null) {
+        if(null != $wd) {
+            $this->wd = $wd;
+        }
+        return $this->wd;
+    }
+
+
+}
---
+++ src/BAS/zf2/Helpers/ConfigHelper.php
@@ -0,0 +1,43 @@
+<?php 
+namespace BAS\zf2\Helpers;
+
+class ConfigHelper 
+{
+    protected $wd;
+
+    protected $application_config;
+    
+    public function __construct() {
+        
+        $this->setWorkingDir(getcwd());
+        $this->setApplicationConfig();
+        
+    }
+
+    protected function setWorkingDir($wd = null) {
+        if(null != $wd) {
+            $this->wd = $wd;
+        }
+        return $this->wd;
+    }
+
+    protected function setApplicationConfig() {
+        
+        if(is_file($this->wd . '/config/application.config.php')) {
+            $this->application_config = include $this->wd . '/config/application.config.php';
+        }
+        return $this->application_config;        
+    }
+
+    public function getApplicationConfig() {
+        if(null != $this->application_config) {
+            return $this->application_config;
+        } else {
+            $this->setApplicationConfig();
+        }
+    }
+
+        
+
+
+}
