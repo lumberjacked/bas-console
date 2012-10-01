012f80e61ecf0fb35de7f6b64b864c463b079432
lumberjacked <lumberjacked.again@gmail.com>
2012-10-01 07:36:52 -0500

    Changed readme to start reflecting the usage of this project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# This is a command line console to generate zf2 files using Symfony console component
+# This is a Command Line Tool to generate zf2 files.  Inspired from Rails command line tool.
+
+## Usage
+
+- Clone the project to a working directory
+git clone git@bitbucket.org:lumberjacked/command.console.git
+
+- Alias the project to access it from anywhere.
+- In .bash_alias add something like alias zf2='~/workspace/command.console/console'
+
+
+
+# Easiest way to get started is to create an Alias to the console file from your cloned project.
+
--- src/BAS/zf2/Commands/ModuleCommand.php
+++ src/BAS/zf2/Commands/ModuleCommand.php
@@ -9,19 +9,24 @@ use Symfony\Component\Console\Command\Command,
 
 class ModuleCommand extends Command 
 {
-    protected function configure()
-    {
+    protected function configure() {
         $this->setName('generate:module')->setDescription('Create a Brand Spanking New zf2 Module in an Exsisting Project!');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $output->writeln("<info>Creating a Module</info>");
         $output->writeln("<comment>Things to Remember</comment>");
         $output->writeln("<error>Required Argument -- Namespace</error>");
         $output->writeln("<error>Required Argument -- Module Name</error>");
         $output->writeln("<error>Maybe doesn't happen here but will need a route to each view file (This is a new command)</error>");
+
+        passthru('whoami');
         
+    }
+
+    protected function createDirectoryStructure() {
+        
+
     }
 
 }
