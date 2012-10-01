4a0de11c6b47c36985a4b8e98a6c590150c4c832
lumberjacked <sbragg.mymobilecms@gmail.com>
2012-09-30 21:02:51 -0500

    Got all of the generate:app logic worked out and generating new apps.  Still some error handling and refactoring needs to be done. Seperation of the sub-execute commands needs to happen.

---
+++ src/BAS/zf2/ApplicationCommand.php
@@ -0,0 +1,9 @@
+<?php
+namespace BAS\zf2;
+
+use Symfony\Component\Console\Command\Command;
+
+
+class ApplicationCommand extends Command {
+
+}
---
+++ src/BAS/zf2/Commands/AppCommand.php
@@ -0,0 +1,50 @@
+<?php 
+namespace BAS\zf2\Commands;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface;
+
+class AppCommand extends Command 
+{
+    protected function configure()
+    {
+        $this->setName('generate:app')->setDescription('Create a Brand Spanking New zf2 Project')
+             ->addArgument('{ProjectName}', InputArgument::REQUIRED, 'Please choose a project name.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $projectName = $input->getArgument('{ProjectName}');
+ 
+        $output->writeln("<info>Cloning New Skeleton Application as {$projectName}</info>");
+        passthru("git clone git://github.com/zendframework/ZendSkeletonApplication.git {$projectName}");
+        $workingDirectory = getcwd() . "/{$projectName}/";    
+        
+        $output->writeln("<info>Now going through and Cleaning up the files that are not needed!</info>");
+        foreach($this->getUnlinkFiles() as $file) {
+            $path = $workingDirectory . $file;
+            if(is_file($path)) {
+                $output->writeln("Deleting File -- <error>{$file}</error>");
+                unlink($path);
+            }
+        }
+        
+        $output->writeln("<comment>Running Composer to install dependencies.</comment>");
+        
+        passthru("cd {$workingDirectory}; php composer.phar install");
+        
+        $output->writeln("<error>Remember to setup the vhost file to run the site!</error>");
+    }
+
+    public function getUnlinkFiles() 
+    {
+        //Am planning on adding the ZF2 directory in vendor because composer install the library in the zendframework directory.  
+        return array('LICENSE.txt',
+                     'README.md',
+                     
+                    );
+    }
+}
