e125bb0ef73e0076a4e14792aa0ec675145cf548
lumberjacked <sbragg.mymobilecms@gmail.com>
2012-09-30 21:17:16 -0500

    Added new file to start on generate:module

---
+++ src/BAS/zf2/Commands/ModuleCommand.php
@@ -0,0 +1,27 @@
+<?php 
+namespace BAS\zf2\Commands;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface;
+
+class ModuleCommand extends Command 
+{
+    protected function configure()
+    {
+        $this->setName('generate:module')->setDescription('Create a Brand Spanking New zf2 Module in an Exsisting Project!');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("<info>Creating a Module</info>");
+        $output->writeln("<comment>Things to Remember</comment>");
+        $output->writeln("<error>Required Argument -- Namespace</error>");
+        $output->writeln("<error>Required Argument -- Module Name</error>");
+        $output->writeln("<error>Maybe doesn't happen here but will need a route to each view file (This is a new command)</error>");
+        
+    }
+
+}
