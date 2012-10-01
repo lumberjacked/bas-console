9bd3386f521848023305bb19ef054c761e47aea6
lumberjacked <lumberjacked.again@gmail.com>
2012-10-01 10:23:00 -0500

    Starting to write out the logic to create a module in a zf2 project

--- src/BAS/zf2/Commands/ModuleCommand.php
+++ src/BAS/zf2/Commands/ModuleCommand.php
@@ -9,8 +9,15 @@ use Symfony\Component\Console\Command\Command,
 
 class ModuleCommand extends Command 
 {
+    
+    protected $_namespace;
+
+    protected $_moduleFile;   
+
     protected function configure() {
-        $this->setName('generate:module')->setDescription('Create a Brand Spanking New zf2 Module in an Exsisting Project!');
+        $this->setName('generate:module')->setDescription('Create a Brand Spanking New zf2 Module in an Exsisting Project!')
+             ->addArgument('{ModuleName}', InputArgument::REQUIRED, 'Please choose a Name for this Module.');
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
@@ -20,13 +27,94 @@ class ModuleCommand extends Command
         $output->writeln("<error>Required Argument -- Module Name</error>");
         $output->writeln("<error>Maybe doesn't happen here but will need a route to each view file (This is a new command)</error>");
 
-        passthru('whoami');
+        $this->setNamespace($input->getArgument('{ModuleName}'));
+
+        $this->createDirectoryStructure();
+
         
     }
 
     protected function createDirectoryStructure() {
         
+        $structure = $this->getModuleDirectoryArray();
+        $workingDirectory = getcwd();
+        var_dump(__DIR__);
+        var_dump($workingDirectory);
+        var_dump($structure);die('structure');        
 
+    }
+
+    protected function getModuleDirectoryArray() {
+        
+        return array(
+            'ModuleName' => array(
+                'config',
+                'src' => array(
+                    'ModuleName' => array(
+                        'Controller',
+                        'Form',
+                        'Model',
+                    ),
+                ),
+                'view' => array(
+                    'ModuleNameLower' => array(
+                        'ControllerNameLower'
+                    )
+                )
+            )                
+        );
+
+    }
+
+    protected function getModuleFile() {
+        
+        if(null !== $this->_moduleFile) {
+            return $this->_moduleFile;
+        } else {
+            $this->setModuleFile();
+            return $this->_moduleFile;
+        }
+    }
+
+    protected function setModuleFile() {
+        $this->_moduleFile =
+<<<MODULE
+// module/{$this->getNamespace()}/Module.php
+namespace {$this->getNamespace()};
+
+class Module {
+                
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . 'autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
+MODULE;
+  
+        return $this->_moduleFile;
+    }
+
+    protected function getNamespace() {
+        return $this->_namespace;
+    }
+
+    protected function setNamespace($namespace = null) {
+        if(null !== $namespace) {
+            $this->_namespace = ucfirst($namespace);
+        }
+        return $this->_namespace;
     }
 
 }
