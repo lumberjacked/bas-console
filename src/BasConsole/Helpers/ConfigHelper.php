d71c7f9b9314adfe6ec839fa87e85768797fa3b4
lumberjacked <sbragg.braggaboutsystems@gmail.com>
2012-10-03 10:18:26 -0500

    Added new method to do a recursive search for a file

--- src/BasConsole/Helpers/ConfigHelper.php
+++ src/BasConsole/Helpers/ConfigHelper.php
@@ -9,8 +9,21 @@ class ConfigHelper
         $this->workingDir = getcwd();
     }
 
-    public function getConfig() {
-        var_dump($this->workingDir);die('working');
+    public function getConfig($moduleName = null) {
+        //var_dump($this->recursiveGlob($this->workingDir, 'application.config.php'));die();
+        var_dump($this->recursiveGlob($this->workingDir, 'module.config.php'));die();
+    }
+
+    public function recursiveGlob($path, $pattern = '*', $flag = 0) {
+        
+        $paths = glob($path.'*', GLOB_MARK|GLOB_ONLYDIR|GLOB_NOSORT);
+        $files = glob($path.$pattern, $flag);
+
+        foreach($paths as $path) {
+            $files = array_merge($files, $this->recursiveGlob($path, $pattern, $flag));
+        }
+
+        return $files;
     }
         
 }
